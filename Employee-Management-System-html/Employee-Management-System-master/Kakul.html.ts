d734ff377de28fc936cc3f43bbdb383f15cec6da
Aditya Sharma <adisharma3030@gmail.com>
2022-01-08 19:37:45 +0530

    first-Commit

---
+++ Employee-Management-System-html/Employee-Management-System-master/Kakul.html.ts
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Employee portal</title>
+</head>
+
+<body>
+
+    <div id="header">
+       
+        <div id="h1right">
+            <h1>Kakul Chaudhary</h1>
+            <p1>I am from Meerut. I am a final year student pursuing MCA at MIET, Meerut.  I spend my free time listening to songs and reading books. I love to explore new places.</p1>
+        </div>
+    </div>
+
+    <div id="content">
+
+     
+
+         </div>
+
+        
+
+               
+
+</body>
+</html>
+
+<button type="button">Click Me!</button>
